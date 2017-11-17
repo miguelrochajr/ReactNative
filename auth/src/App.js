7007c0e5a8d40afb08457880692dc3a6a9ff00aa
Miguel Rocha Jr <miguelsrochajr@gmail.com>
2017-11-17 08:26:08 -0300

    Handling Authentication Events

--- auth/src/App.js
+++ auth/src/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
 import { View } from 'react-native';
 import firebase from 'firebase';
-import { Header } from './components/common';
+import { Header, Button, CardSection, Spinner } from './components/common';
 import LoginForm from './components/LoginForm';
 
 class App extends Component {
+  state = { loggedIn: null };
+
   componentWillMount() {
     firebase.initializeApp({
       apiKey: 'AIzaSyCzlCNDfsn2tkSn19miQXRaCziuQXG6zhg',
@@ -14,12 +16,39 @@ class App extends Component {
       storageBucket: 'authentication-13380.appspot.com',
       messagingSenderId: '196897110535'
     });
+
+    firebase.auth().onAuthStateChanged((user) => {
+      if (user) {
+        this.setState({ loggedIn: true });
+      } else {
+        this.setState({ loggedIn: false });
+      }
+    });
   }
+
+  renderContent() {
+    switch (this.state.loggedIn) {
+      case true:
+        return (
+          <CardSection>
+            <Button>
+              Log Out
+            </Button>
+          </CardSection>
+        );
+      case false:
+        return <LoginForm />;
+      default:
+        return <Spinner size="large" />
+
+    }
+  }
+
   render() {
     return (
       <View>
         <Header headerText="Authetication" />
-        <LoginForm />
+        {this.renderContent()}
       </View>
     );
   }
